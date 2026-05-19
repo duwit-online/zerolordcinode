eca05aa8991c89c9be8fc01cdfd25c1b2b84c03c
duwit-online <duwit.online.dev@gmail.com>
2026-05-19 17:14:02 +0100

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,15 +1,19 @@
+// capacitor.config.ts
+
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.cinode',
   appName: 'Cinode',
   webDir: 'dist',
-  server: {
-    url: 'https://00a31809-aafa-4fea-a3b7-2b8bfaac4ee5.lovableproject.com?forceHideBadge=true',
-    cleartext: true,
+
+  android: {
+    allowMixedContent: true,
   },
-  android: { allowMixedContent: true },
-  ios: { contentInset: 'always' },
+
+  ios: {
+    contentInset: 'always',
+  },
 };
 
 export default config;
